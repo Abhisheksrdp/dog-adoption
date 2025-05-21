92a21d10415cc2b5f8a77837d3d95e6ffabd2d79
Abhisheksrdp <abhisheksrdy555@gmail.com>
2025-05-21 16:23:28 +0530

    "second commit"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-md px-6 py-4 flex items-center justify-between">
       
-      {/* Logo */}
+      
       <div className="text-3xl font-bold text-orange-500 flex items-center space-x-1">
         <span className="text-orange-200">Z</span>
         <span className="text-cyan-500">aan</span>
         <span className="text-cyan-400 font-extralight -ml-2">var</span>
       </div>
 
-      {/* Hamburger for mobile */}
+      
       <button
         className="block md:hidden text-gray-700 focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}
@@ -36,7 +36,7 @@ const Navbar = () => {
         </svg>
       </button>
 
-      {/* Menu Links */}
+      
       <ul
         className={`flex flex-col md:flex-row md:space-x-10 text-md font-semibold text-gray-800 absolute md:static bg-white md:bg-transparent left-0 w-full md:w-auto md:items-center md:justify-center transition-all duration-300 ease-in-out ${
           isOpen ? 'top-16 border-t border-gray-200' : 'top-[-490px]'
@@ -49,7 +49,7 @@ const Navbar = () => {
         <li className="cursor-pointer hover:text-cyan-500 px-4 py-2 md:p-0">About us</li>
       </ul>
 
-      {/* Login Button */}
+      
       <button
         className="hidden md:block border-2 border-cyan-400 text-cyan-500 font-bold px-5 py-1.5 rounded-full hover:bg-cyan-100"
         onClick={() => navigate('/adopt')}
@@ -57,7 +57,7 @@ const Navbar = () => {
         LOG IN
       </button>
 
-      {/* Login Button for mobile */}
+      
       {isOpen && (
         <button
           className="block md:hidden mt-2 w-full border-2 border-cyan-400 text-cyan-500 font-bold px-5 py-2 rounded-full hover:bg-cyan-100"
